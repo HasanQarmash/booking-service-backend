af665c51f6f442cf2a32f8a97a83dd5b01af3255
AbdalrhmanJuber <1211769@student.birzeit.edu>
2025-11-02 21:31:47 +0200

    add: Gmail sending

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,7 +7,7 @@ import {
   EmailError,
   NotFoundError,
 } from "../utils/errors";
-import { sendEmail } from "../utils/email";
+import { Email } from "../utils/email";
 import crypto from "crypto";
 import { createSendToken } from "../utils/createSendtoken";
 
@@ -52,17 +52,12 @@ export class AuthController {
     const resetToken = await this.userModel.createPasswordResettoken(
       user.email,
     );
-
     const resetURL = `${process.env.FRONTEND_URL}/customer/new-password?token=${resetToken}`;
 
-    const message = `Forgot your password? Submit a PATCH request with your new password to: ${resetURL}.
-If you didn't request a password reset, please ignore this email.`;
     try {
-      await sendEmail({
-        to: user.email,
-        subject: "Your password reset token (valid for 15 min)",
-        text: message,
-      });
+      // 📨 Use the Email class
+      const emailService = new Email(user, resetURL);
+      await emailService.sendPasswordReset();
 
       res.status(200).json({
         message: "Password reset link sent successfully",
